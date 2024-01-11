b9a694d2b288b19920da5903535be3e24fd56b69
HP <HP@Sinchana>
2024-01-11 14:28:49 +0530

    First Commit

---
+++ Day01/src/org/tnsif/dayzeroone/basic/BreakDemo.java
@@ -0,0 +1,11 @@
+package org.tnsif.dayzeroone.basic;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
